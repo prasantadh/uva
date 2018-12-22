8f6c5a5be9f927a05a94b8555da4d509f6e0eeae
prasant adhikari <pa1038@nyu.edu>
2018-12-22 18:43:16 +0400

    initial template file

---
+++ template.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    cout << "This is initial template" << endl;
+    cout << "Build on this as needed" << endl;
+    return 0;
+}
