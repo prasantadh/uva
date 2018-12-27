5c577a009f8ef4e6bef6f09e7be8cbdc0b3d4f8b
prasant adhikari <pa1038@nyu.edu>
2018-12-27 15:49:20 +0400

    10576 Y2K Accounting Bug - Recursive Backtracking
    rewrote the code to use recursion instead of bitmask for subset
    enumeration

--- 10576/main.cpp
+++ 10576/main.cpp
@@ -1,27 +1,43 @@
 #include <iostream>
 
+#define INF (1 << 29)
+
 using namespace std;
 
+int s, d, ans, acc[12]; // global states
+
+/**
+ * @params
+ * month:   the current month under consideration
+ * balance: the balance for current month (could be s or d)
+ * @return: 
+ *          the best possible surplus at the end of the year
+ */
+int solve(int month, int balance) {
+    acc[month] = balance;
+    int total = 0;
+    if (month > 3) {
+        for(int i = month - 4; i <= month; ++i)
+            total += acc[i];
+        if (total > 0) return -INF;
+    }
+    if (month == 11) {
+        for(int i = 0; i < month - 4; ++i) total += acc[i];
+        return total;
+    }
+    
+    int withs = solve(month + 1, s);
+    int withd = solve(month + 1, d);
+    return max(withs, withd);
+}
+
 int main(int argc, char **argv) {
-    int s, d, ans;
     while(cin >> s >> d) {
         d = -d, ans = -1;
-        for(int mask = 1; mask < (1 << 12); ++mask) {
-            int total, i;
-            for (i = 4; i < 12; ++i) {
-                total = 0;
-                for(int j = i - 4; j <= i; ++j)
-                    total += ((1 << j) & mask) ? s : d;
-                if (total > 0) break;
-            }
-            if (i == 12) {
-                total = 0;
-                for(int j = 0; j < 12; ++j)
-                    total += ((1 << j) & mask) ? s : d;
-                ans = max(total, ans);
-            }
-        }
-        if (ans == -1)
+        int with_s = solve(0, s);
+        int with_d = solve(0, d);
+        ans = max(with_s, with_d);
+        if (ans < 0)
             cout << "Deficit" << endl;
         else
             cout << ans << endl;
