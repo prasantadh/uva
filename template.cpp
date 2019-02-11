aabd52a7390a64eadef8d8d754686df19b31cb7a
prasant adhikari <pa1038@nyu.edu>
2019-02-11 11:12:02 +0400

    Updated template file
    with vector inclusion, INF definition and typedef ii for pair<int, int>

--- template.cpp
+++ template.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <vector>
+
+#define INF (1 << 30)
 
 using namespace std;
+
+typedef pair<int, int> ii;
 
 int main(int argc, char **argv) {
     cout << "This is initial template" << endl;
